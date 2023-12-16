ba5285fa1e927d00469399b5136f997e2f4eef6c
kavindumal <kavindu11250403@gmail.com>
2023-12-16 14:41:33 +0530

    all finished

--- src/main/java/com/example/layeredarchitecture/controller/ManageItemsFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/ManageItemsFormController.java
@@ -1,10 +1,7 @@
 package com.example.layeredarchitecture.controller;
 
 import com.example.layeredarchitecture.dao.ItemDAOImpl;
-import com.example.layeredarchitecture.db.DBConnection;
-import com.example.layeredarchitecture.model.CustomerDTO;
 import com.example.layeredarchitecture.model.ItemDTO;
-import com.example.layeredarchitecture.view.tdm.CustomerTM;
 import com.example.layeredarchitecture.view.tdm.ItemTM;
 import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
@@ -24,7 +21,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URL;
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ManageItemsFormController {

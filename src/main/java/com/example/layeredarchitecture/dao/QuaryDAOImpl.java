d14d0aa31af6ffb50a0a191bceda3fdc6fb9ba6c
kavindumal <kavindu11250403@gmail.com>
2023-12-18 16:12:57 +0530

    QuaryDAOImpl.java impl

---
+++ src/main/java/com/example/layeredarchitecture/dao/QuaryDAOImpl.java
@@ -0,0 +1,11 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+public class QuaryDAOImpl implements QueryDAO {
+
+    @Override
+    public void customerOrderDetails(CustomerDTO customerDTO) {
+
+    }
+}
